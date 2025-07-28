d16a9bdd0d730bc6b9b9fd4f733dba5396905dea
Gabriel de Aguiar Féo <gfeo@gradle.com>
2025-07-28 20:41:57 +0100

    Update test devices API level

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/GradleManagedDevices.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/GradleManagedDevices.kt
@@ -27,9 +27,9 @@ import org.gradle.kotlin.dsl.invoke
 internal fun configureGradleManagedDevices(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-    val pixel4 = DeviceConfig("Pixel 4", 30, "aosp-atd")
-    val pixel6 = DeviceConfig("Pixel 6", 31, "aosp")
-    val pixelC = DeviceConfig("Pixel C", 30, "aosp-atd")
+    val pixel4 = DeviceConfig("Pixel 4", 35, "aosp-atd")
+    val pixel6 = DeviceConfig("Pixel 6", 34, "aosp-atd")
+    val pixelC = DeviceConfig("Pixel C", 33, "aosp-atd")
 
     val allDevices = listOf(pixel4, pixel6, pixelC)
     val ciDevices = listOf(pixel4, pixelC)
